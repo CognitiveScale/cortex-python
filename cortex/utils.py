d22cb18e92cdfbe2a1474865e17ecf62b8487fc2
Dave Wisecup <dwisecup@cognitivescale.com>
2020-03-05 14:23:06 -0600

    accidently removed line

--- cortex/utils.py
+++ cortex/utils.py
@@ -152,7 +152,7 @@ def get_logger(name):
     log = logging.getLogger(name)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s/%(module)s: %(message)s')
     handler = logging.StreamHandler()
-
+    handler.setFormatter(formatter)
     log.addHandler(handler)
     log.setLevel(logging.INFO)
     return log
