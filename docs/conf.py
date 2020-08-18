024aa1d1df4ff3b88101258e85b2d529f7c5d82f
clee <clee@cognitivescale.com>
2020-08-18 18:48:04 -0500

    moving git import

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 import sphinx_rtd_theme
+from git import Repo
 
 
 # -- General configuration ------------------------------------------------
@@ -218,7 +219,6 @@ else:
 html_context['current_language'] = current_language
 
 # SET CURRENT_VERSION
-from git import Repo
 repo = Repo( search_parent_directories=True )
 
 if 'current_version' in os.environ:
