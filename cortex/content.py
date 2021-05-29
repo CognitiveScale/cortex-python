16b5e7286c0b27fb3f653c3d1f445dd4b84c5587
Johan Gielstra <jgielstra@cognitivescale.com>
2021-05-29 08:30:35 -0500

    Support both args/kwargs, allow project to be optional

--- cortex/content.py
+++ cortex/content.py
@@ -37,10 +37,11 @@ class ManagedContentClient(_Client):
         super().__init__(*args, **kwargs)
         self._serviceconnector.version = 4
 
-    def upload(self, key: str, project: str, stream_name: str, stream: object, content_type: str, retries: int = 1):
+    def upload(self, *args, **kwargs):
         """Store `stream` file in S3.
 
         :param key: The path where the file will be stored.
+        :param project: The project to upload
         :param stream_name: The name under which to save the `stream`.
         :param stream: The file object.
         :param content_type: the type of the file to store (e.g., `text/csv`).
@@ -50,10 +51,31 @@ class ManagedContentClient(_Client):
         .. NOTE: This method uses a multi-part form request; to upload very large files, use `uploadStreaming` instead.
         .. seealso: uploadStreaming()
         """
+        key = kwargs.get('key')
+        project = kwargs.get('project', self._serviceconnector.project)
+        stream_name = kwargs.get('stream_name')
+        stream = kwargs.get('stream')
+        content_type = kwargs.get('content_type')
+        retries = kwargs.get('retries', 1)
+        if len(args) > 0:
+            if len(args) == 6:
+                retries = args[5]
+            if len(args) == 5:
+                project = args[1]
+            if len(args) == 4:
+                if not key:
+                    key = args[0]
+                if not stream:
+                    stream = args[3]
+                if not stream_name:
+                    stream_name = args[2]
+                if not content_type:
+                    content_type = args[4]
+
         r = tenacity.Retrying(
-            stop = tenacity.stop_after_attempt(retries + 1),
-            retry = tenacity.retry_if_exception(ManagedContentClient._http_request_retry_predicate)
-            )
+                stop = tenacity.stop_after_attempt(retries + 1),
+                retry = tenacity.retry_if_exception(ManagedContentClient._http_request_retry_predicate)
+                )
         return r.wraps(self._upload)(key, project, stream_name, stream, content_type)
 
     def _upload(self, key: str, project: str, stream_name: str, stream: object, content_type: str):
@@ -75,18 +97,32 @@ class ManagedContentClient(_Client):
                     'relative': path[len(source):]
                 }
 
-    def upload_directory(self, source: str, destination: str, project:str, retries: int = 1):
+    def upload_directory(self, *args, **kwargs):
         """Walk source directory and store in S3
 
         :param source: The path to the local directory.
-        :param destination: Prefix to add to resulting saved directory.
+        :param destination: Prefix to add to resulting saved directory
+        :param project: The project to store the content within.
         :param retries: Number of times to retry a failed request from a retryable response.
         :return: A dict with the response to request upload.
         """
+        source = kwargs.get('source')
+        destination = kwargs.get('destination')
+        project = kwargs.get('project', self._serviceconnector.project)
+        retries = kwargs.get('retries', 1)
+        if len(args) > 0:
+            if len(args) == 4:
+                retries = args[3]
+            if len(args) == 3:
+                project = args[2]
+            if len(args) == 2:
+                if not source:
+                    source = args[0]
+                if not destination:
+                    destination = args[1]
         source_path = source
         if not source.endswith('/'):
             source_path = source_path + '/'
-
         generated_paths = ManagedContentClient._get_source_files(source_path)
         responses = []
         for path_dict in generated_paths:
@@ -96,16 +132,33 @@ class ManagedContentClient(_Client):
 
         return responses
 
-    def upload_streaming(self, key: str, project: str, stream: object, content_type: str, retries: int = 1):
+    def upload_streaming(self, *args, ** kwargs): # key: str, project: str, stream: object, content_type: str, retries: int = 1):
         """Store `stream` file in S3.
 
         :param key: The path where the file will be stored.
-        :param project: The project to which file will be stored.
+        :param project: The project to store the content within.
         :param stream: The file object.
         :param content_type: The type of the file to store (e.g., `text/csv`)
         :param retries: Number of times to retry a failed request from a retryable response.
         :return: A dict with the response to request upload.
         """
+        key = kwargs.get('key')
+        project = kwargs.get('project', self._serviceconnector.project)
+        stream = kwargs.get('stream')
+        content_type = kwargs.get('content_type')
+        retries = kwargs.get('retries', 1)
+        if len(args) > 0:
+            if len(args) == 5:
+                retries = args[4]
+            if len(args) == 4:
+                project = args[1]
+            if len(args) == 3:
+                if not key:
+                    key = args[0]
+                if not stream:
+                    stream = args[1]
+                if not content_type:
+                    content_type = args[2]
         r = tenacity.Retrying(
             stop = tenacity.stop_after_attempt(retries + 1),
             retry = tenacity.retry_if_exception(ManagedContentClient._http_request_retry_predicate)
@@ -119,7 +172,7 @@ class ManagedContentClient(_Client):
         raise_for_status_with_detail(r)
         return r.json()
 
-    def download(self, key: str, project, retries: int = 1):
+    def download(self, *args, **kwargs):
         """Download a file from managed content (S3).
 
         :param key: The path of the file to retrieve.
@@ -127,6 +180,16 @@ class ManagedContentClient(_Client):
         :param retries: Number of times to retry a failed request from a response.
         :returns: A Generator.
         """
+        key = kwargs.get('key')
+        project = kwargs.get('project', self._serviceconnector.project)
+        retries = kwargs.get('retries', 1)
+        if len(args) > 0:
+            if len(args) == 3:
+                retries = args[2]
+            if len(args) == 2:
+                project = args[1]
+            if not key:
+                key = args[0]
         r = tenacity.Retrying(
             stop = tenacity.stop_after_attempt(retries + 1),
             retry = tenacity.retry_if_exception(ManagedContentClient._http_request_retry_predicate)
@@ -139,12 +202,20 @@ class ManagedContentClient(_Client):
         raise_for_status_with_detail(r)
         return r.raw
 
-    def exists(self, key: str, project) -> bool:
+    def exists(self, *args, **kwargs) -> bool:
         """Check that a file from managed content (S3) exists.
 
         :param key: The path of the file to check.
+        :param project: The project to check.
         :returns: A boolean indicating wether the file exists or not.
         """
+        key = kwargs.get('key')
+        project = kwargs.get('project', self._serviceconnector.project)
+        if len(args) > 0:
+            if len(args) == 2:
+                project = args[1]
+            if not key:
+                key = args[0]
         uri = self._make_content_uri(key, project)
         r = self._serviceconnector.request('HEAD', uri)
         return r.status_code == 200
