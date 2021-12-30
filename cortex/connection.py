3aafd77d3520450cb17ba84c0610c4f1de7b9c73
dvasani-CS <dvasani@cognitivescale.com>
2021-12-30 14:41:45 +0530

    cleaned-up connection client

--- cortex/connection.py
+++ cortex/connection.py
@@ -1,4 +1,3 @@
-
 """
 Copyright 2021 Cognitive Scale, Inc. All Rights Reserved.
 
@@ -18,7 +17,7 @@ limitations under the License.
 import json
 import os
 import urllib.parse
-from .serviceconnector import _Client, ServiceConnector
+from .serviceconnector import _Client
 from .camel import CamelResource
 from .utils import get_logger
 from .utils import raise_for_status_with_detail
@@ -35,53 +34,50 @@ class ConnectionClient(_Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._serviceconnector.version = 4
+        if "project" in kwargs:
+            self._project = kwargs.get("project")
 
-    def save_connection(self, project: str, connection: object):
+    def save_connection(self, connection: object):
         """
         Posts the connection client information.
+        :param connection: Connection object
+        :return: status
         """
-        uri = self.URIs['connections'].format(projectId=project)
+        uri = self.URIs['connections'].format(projectId=self._project)
         data = json.dumps(connection)
         headers = {'Content-Type': 'application/json'}
         r = self._serviceconnector.request('POST', uri, data, headers)
         raise_for_status_with_detail(r)
         return r.json()
 
-    ## Private ##
+    def get_connection(self, name: str):
+        """
+        Fetches a Connection to work with.
+        :param name: The name of the connection to retrieve.
+        :return: A Connection object.
+        """
+        port = os.getenv('CORTEX_CONNECTIONS_SERVICE_PORT_HTTP_CORTEX_CONNECTIONS') or '4450'
+        conn_svc_url = f'{self._serviceconnector.url.replace("cortex-internal", "cortex-connections")}:{port}'
+        uri = f'{conn_svc_url}/internal/projects/{self._project}/connections/{urllib.parse.quote(name, safe="")}'
+        log.debug('Getting connection using URI: %s' % uri)
+        r = self._serviceconnector.request('GET', uri, is_internal_url=True)
+        raise_for_status_with_detail(r)
+
+        return r.json()
 
     def _bootstrap(self):
-        uri  = self.URIs['connections'] + '/_/bootstrap'
+        uri = self.URIs['connections'] + '/_/bootstrap'
         r = self._serviceconnector.request('GET', uri)
         raise_for_status_with_detail(r)
         return r.json()
 
 
 class Connection(CamelResource):
-
     """
     Defines the connection for a dataset.
     """
 
-    def __init__(self, connection, connector: ServiceConnector):
+    def __init__(self, connection, project: str, client: ConnectionClient):
         super().__init__(connection, True)
-        self._connector = connector
-
-
-    @staticmethod
-    def get_connection(name, project, client: ConnectionClient):
-        """
-        Fetches a Connection to work with.
-
-        :param client: The client instance to use.
-        :param name: The name of the connection to retrieve.
-        :param project: The project from which connection has to be retrieved.
-        :return: A Connection object.
-        """
-        port = os.getenv('CORTEX_CONNECTIONS_SERVICE_PORT_HTTP_CORTEX_CONNECTIONS') or '4450'
-        conn_svc_url = f'{client._serviceconnector.url.replace("cortex-internal", "cortex-connections")}:{port}'
-        uri = f'{conn_svc_url}/internal/projects/{project}/connections/{urllib.parse.quote(name, safe="")}'
-        log.debug('Getting connection using URI: %s' % uri)
-        r = client._serviceconnector.request('GET', uri, is_internal_url=True)
-        raise_for_status_with_detail(r)
-
-        return r.json()
+        self._client = client
+        self._project = project
