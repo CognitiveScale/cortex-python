e02af2ea57456a7cb7dc56d3582a5a925ab810a0
Johan Gielstra <jgielstra@cognitivescale.com>
2022-11-04 12:15:45 -0500

    bump version 3.2.0

--- cortex/__version__.py
+++ cortex/__version__.py
@@ -4,7 +4,7 @@ Metadata for cortex-python
 __title__ = 'cortex-python'
 __description__ = 'Python module for the CognitiveScale Cortex Cognitive Platform'
 __url__ = 'https://github.com/CognitiveScale/cortex-python'
-__version__ = '6.1.0'
+__version__ = '6.2.0'
 __author__ = 'CognitiveScale'
 __author_email__ = 'support@cognitivescale.com'
 __license__ = 'Apache 2.0'
