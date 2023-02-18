efbd94e900ff6481c5c0eb8d58fd45023d21f325
Prajna Kandarpa <pkandarpa@cognitivescale.com>
2023-02-19 01:06:47 +0530

    slightly better docs for message.py

--- cortex/message.py
+++ cortex/message.py
@@ -24,25 +24,28 @@ log = get_logger(__name__)
 
 
 class Message(Document):
+    # pylint: disable=line-too-long
     """
     Wraps a set of parameters or a payload when invoking an action, skill, or agent.
 
-    params:
-    activationId: Request ID for the current execution
-    agentName: Name of the agent invoking the skill, will be empty for skill invokes
-    apiEndpoint: URI of the API server to use when making requests to
-    platform services in processing of this message.
-    channelId: ID of the channel (wire) this message was sent on
-    outputName: Output name defined in the agent definition, can be overridden
-    payload: JSON payload passed to the skill
-    projectId: Name of the project invoking this skill
-    properties: Properties merged from Skill definition, Skill reference, and Agent definition.
-    sessionId: The ID of the session associated with this message,
-    this will be the activationId unless provided externally
-    skillName: The name of the skill being invoked.
-    timestamp: Timestamp of the invoke
-    token: The JWT token to be used for making authenticated requests to APIs
-    needed to process this message.
+    The following keys are valid in the params dictionary
+    .. code-block::
+        {
+            "activationId": "Request ID for the current execution",
+            "agentName": "Name of the agent invoking the skill, will be empty for skill invokes",
+            "apiEndpoint": "URI of the API server to use when making requests to
+        platform services in processing of this message.",
+            "channelId": "ID of the channel (wire) this message was sent on",
+            "outputName": "Output name defined in the agent definition, can be overridden",
+            "payload": "JSON payload passed to the skill",
+            "projectId": "Project to which this skill belongs to",
+            "properties": "Properties merged from Skill definition, Skill reference, and Agent definition.",
+            "sessionId": "The ID of the session associated with this message,
+        this will be the activationId unless provided externally",
+            "skillName": "The name of the skill being invoked.",
+            "timestamp": "Timestamp of the invoke",
+            "token": " The JWT token to be used for making authenticated requests to APIs needed to process this message."
+        }
     """
 
     def __init__(self, params: Dict = None):
@@ -66,10 +69,10 @@ class Message(Document):
 
     @staticmethod
     def from_env(**kwargs):
-        """_summary_
+        """Creates an instance of :class:`cortex.message.Message` by reading from existing environment and cortex profile
 
-        Returns:
-            _type_: _description_
+        :return: :class:`cortex.message.Message` pre-populated with params loaded from pre-existing Cortex environment variables or Cortex profile
+        :rtype: :class:`cortex.message.Message`
         """
         env = CortexEnv(**kwargs)
 
