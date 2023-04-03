1176303d46563ae7ba4c01670b5b62a432d3e4c3
Prajna Kandarpa <pkandarpa@cognitivescale.com>
2023-04-03 10:44:35 +0530

    up the min version for python-jwt

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     install_requires=[
         "requests>=2.12.4,<3",
         "requests-toolbelt>=0.8.0",
-        "python-jwt>=3.3.0,<4.0",
+        "python-jwt>=3.3.5,<4.0",
         "pyyaml>=5.3.1,<6",
         "cuid>=0.3,<1",
         "dill>=0.2.8.2",
