5fb3671615d655dab8dd3aa14ec2afcd63650bd6
Luis Aguirre <laguirre@cognitivescale.com>
2023-07-13 10:45:54 -0500

    Bump package to 6.4.0

--- cortex/__version__.py
+++ cortex/__version__.py
@@ -4,7 +4,7 @@ Metadata for cortex-python
 __title__ = "cortex-python"
 __description__ = "Python module for the CognitiveScale Cortex Cognitive Platform"
 __url__ = "https://github.com/CognitiveScale/cortex-python"
-__version__ = "6.3.1"
+__version__ = "6.4.0"
 __author__ = "CognitiveScale"
 __author_email__ = "support@cognitivescale.com"
 __license__ = "Apache 2.0"
